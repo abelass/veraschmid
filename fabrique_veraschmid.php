4a06def6e5709fa34c3c66f564ebd421f0487b48
Rainer Müller <rainer@websimple.be>
2019-07-09 16:20:37 +0200

    import initial

---
+++ fabrique_veraschmid.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ *  Fichier généré par la Fabrique de plugin v6
+ *   le 2019-07-09 16:18:41
+ *
+ *  Ce fichier de sauvegarde peut servir à recréer
+ *  votre plugin avec le plugin «Fabrique» qui a servi à le créer.
+ *
+ *  Bien évidemment, les modifications apportées ultérieurement
+ *  par vos soins dans le code de ce plugin généré
+ *  NE SERONT PAS connues du plugin «Fabrique» et ne pourront pas
+ *  être recréées par lui !
+ *
+ *  La «Fabrique» ne pourra que régénerer le code de base du plugin
+ *  avec les informations dont il dispose.
+ *
+**/
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$data = array (
+  'fabrique' => 
+  array (
+    'version' => 6,
+  ),
+  'paquet' => 
+  array (
+    'prefixe' => 'veraschmid',
+    'nom' => 'Vera Schmid',
+    'slogan' => 'Squelettes pour le site veraschmid.com',
+    'description' => '',
+    'logo' => 
+    array (
+      0 => '',
+    ),
+    'version' => '1.0.0',
+    'auteur' => 'Rainer',
+    'auteur_lien' => 'https://web-simple.eu',
+    'licence' => 'GNU/GPL v3',
+    'categorie' => 'squelette',
+    'etat' => 'dev',
+    'compatibilite' => '[3.2.4;3.2.*]',
+    'documentation' => 'https://github.com/abelass/veraschmid',
+    'administrations' => '',
+    'schema' => '1.0.0',
+    'formulaire_config' => '',
+    'formulaire_config_titre' => '',
+    'inserer' => 
+    array (
+      'paquet' => '',
+      'administrations' => 
+      array (
+        'maj' => '',
+        'desinstallation' => '',
+        'fin' => '',
+      ),
+      'base' => 
+      array (
+        'tables' => 
+        array (
+          'fin' => '',
+        ),
+      ),
+    ),
+    'scripts' => 
+    array (
+      'pre_copie' => '',
+      'post_creation' => '',
+    ),
+    'exemples' => '',
+  ),
+  'objets' => 
+  array (
+  ),
+  'images' => 
+  array (
+    'paquet' => 
+    array (
+      'logo' => 
+      array (
+        0 => 
+        array (
+          'extension' => '',
+          'contenu' => '',
+        ),
+      ),
+    ),
+    'objets' => 
+    array (
+    ),
+  ),
+);
---
+++ lang/paquet-veraschmid_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// V
+	'veraschmid_description' => '',
+	'veraschmid_nom' => 'Vera Schmid',
+	'veraschmid_slogan' => 'Squelettes pour le site veraschmid.com',
+);
