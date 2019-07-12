1a1409deddc62dc54441b23d4937a8190e193a0d
Rainer Müller <rainer@websimple.be>
2019-07-12 16:55:26 +0200

    les variables de base plus les icônes sociaux

---
+++ veraschmid_pipelines.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Utilisations de pipelines par Vera Schmid
+ *
+ * @plugin     Vera Schmid
+ * @copyright  2019
+ * @author     Rainer Müller
+ * @licence    GNU/GPL v3
+ * @package    SPIP\Veraschmid\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
